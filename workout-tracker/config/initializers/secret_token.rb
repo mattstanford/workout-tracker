f8ab990521f327b241d05f94296fa34f6bc80b90
Matt Stanford <mds6058@gmail.com>
2013-12-18 20:17:21 -0500

    Added rails instance

---
+++ workout-tracker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkoutTracker::Application.config.secret_token = 'a7a98e32d8c099275daad94d94a85732199897e944eafb5d1ea0d032e8be295af92c018a047f0a725e24b122e6aaa9aad1545a25f52a71e2f79d814be3a6e9c3'
