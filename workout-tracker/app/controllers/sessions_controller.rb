208712ab3e99c9bddab387b136448d941264be83
Matt Stanford <mds6058@gmail.com>
2013-12-30 22:24:56 -0500

    Initial user session handling

--- workout-tracker/app/controllers/sessions_controller.rb
+++ workout-tracker/app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user
+      # Sign in
+      puts "found " + user.name
+      sign_in user
+      redirect_to user
+    else
+      # Error
+      render 'new'
+    end
   end
 
   def destroy
--- workout-tracker/app/helpers/sessions_helper.rb
+++ workout-tracker/app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+
+  def sign_in(user)
+    session[:user] = user.id
+    self.current_user = user
+  end
+  
+   def signed_in?
+    !self.current_user.nil?
+  end
+
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by_id(session[:user])
+  end
 end
