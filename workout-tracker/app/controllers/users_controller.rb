31f6aa6fe6d42dd19f850f5b1a63466dd5a51360
Matt Stanford <mds6058@gmail.com>
2014-01-02 18:42:11 -0500

    Added deleting of users

--- workout-tracker/app/controllers/users_controller.rb
+++ workout-tracker/app/controllers/users_controller.rb
@@ -34,4 +34,10 @@ class UsersController < ApplicationController
   def index
     @users = User.paginate(page: params[:page])
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_url
+  end
 end
