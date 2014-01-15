1cd91bd06644528914b120e1a88d239bccfa7828
Matt Stanford <mds6058@gmail.com>
2014-01-15 00:43:07 -0500

    Datepicker UI widget submits the run date properly

--- workout-tracker/app/assets/javascripts/runs.js
+++ workout-tracker/app/assets/javascripts/runs.js
@@ -1,13 +1,20 @@
 
 window.onload = function () {
-  $("#datepicker").datepicker({
+  $("#run_date").datepicker({
     beforeShow: function (input, inst) {
+        
         setTimeout(function () {
             inst.dpDiv.css({
                 top: input.offsetTop,
                 left: input.offsetLeft + input.offsetWidth + 20
             });
         }, 0);
-    }
+        
+        
+    },
+    
+    // Rails needs the format like this for the 'Date' format
+    dateFormat: 'yy-mm-dd'
+    
 });
 }
